64b39016626cd68cc2c40c8952d312ef74626274
Naser Sami <ponanzar@gmail.com>
2022-01-28 15:43:21 +0200

    sorting post data in firebase

---
+++ lib/resources/firestore_methods.dart
@@ -0,0 +1,49 @@
+import 'dart:typed_data';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:instagram_clone/models/post.dart';
+import 'package:instagram_clone/resources/storage_methods.dart';
+import 'package:uuid/uuid.dart';
+
+class FirestoreMethods {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  // Upload post
+  Future<String> uploadPost(
+    String description,
+    Uint8List file,
+    String uid,
+    String username,
+    String profImage,
+  ) async {
+    String res = "Some error occurred";
+
+    try {
+      String photoUrl =
+          await StorageMethods().uploadImageToStorage('posts', file, true);
+
+      String postId = const Uuid().v1();
+
+      Post post = Post(
+        // set the arguments
+        description: description,
+        uid: uid,
+        username: username,
+        postId: postId,
+        datePublished: DateTime.now(),
+        postUrl: photoUrl,
+        profImage: profImage,
+        likes: [],
+      );
+
+      // firebase collection
+      _firestore.collection('posts').doc(postId).set(post.toJason());
+
+      res = "success";
+    } catch (err) {
+      res = err.toString();
+    }
+
+    return res;
+  }
+}
--- lib/screens/add_post_screen.dart
+++ lib/screens/add_post_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:instagram_clone/models/user.dart';
 import 'package:instagram_clone/providers/user_provider.dart';
+import 'package:instagram_clone/resources/firestore_methods.dart';
 import 'package:instagram_clone/utils/colors.dart';
 import 'package:instagram_clone/utils/utils.dart';
 import 'package:provider/provider.dart';
@@ -18,6 +19,31 @@ class AddPostScreen extends StatefulWidget {
 class _AddPostScreenState extends State<AddPostScreen> {
   Uint8List? _file;
   final TextEditingController _descriptionController = TextEditingController();
+  bool _isLoading = false;
+
+  void postImage(
+    // arguments
+    String uid,
+    String username,
+    String profImage,
+  ) async {
+    setState(() => _isLoading = true);
+    try {
+      String res = await FirestoreMethods().uploadPost(
+          _descriptionController.text, _file!, uid, username, profImage);
+
+      if (res == "success") {
+        setState(() => _isLoading = false);
+        showSnackBar('Posted!', context);
+        clearImage();
+      } else {
+        setState(() => _isLoading = false);
+        showSnackBar(res, context);
+      }
+    } catch (e) {
+      showSnackBar(e.toString(), context);
+    }
+  }
 
   _selectImage(BuildContext context) async {
     return showDialog(
@@ -59,6 +85,18 @@ class _AddPostScreenState extends State<AddPostScreen> {
     );
   }
 
+  void clearImage() {
+    setState(() {
+      _file = null;
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _descriptionController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     final User user = Provider.of<UserProvider>(context).getUser;
@@ -75,13 +113,14 @@ class _AddPostScreenState extends State<AddPostScreen> {
               backgroundColor: mobileBackgroundColor,
               leading: IconButton(
                 icon: const Icon(Icons.arrow_back),
-                onPressed: () {},
+                onPressed: clearImage,
               ),
               title: const Text("Post to"),
               centerTitle: false,
               actions: [
                 TextButton(
-                  onPressed: () {},
+                  onPressed: () =>
+                      postImage(user.uid, user.username, user.photoUrl),
                   child: const Text(
                     "Post",
                     style: TextStyle(
@@ -95,6 +134,10 @@ class _AddPostScreenState extends State<AddPostScreen> {
             ),
             body: Column(
               children: [
+                _isLoading
+                    ? const LinearProgressIndicator()
+                    : const Padding(padding: EdgeInsets.only(top: 0)),
+                const Divider(),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
                   crossAxisAlignment: CrossAxisAlignment.start,
